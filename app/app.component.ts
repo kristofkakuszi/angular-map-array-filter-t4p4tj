61bad461479603ce43e2eeba4fb6fcd0d1cd723b
kristofkakuszi <60655911+kristofkakuszi@users.noreply.github.com>
2023-04-02 19:28:46 +0200

    map, reduce, filter. Filter is in process

--- app/app.component.ts
+++ app/app.component.ts
@@ -43,6 +43,8 @@ export class AppComponent {
     console.log('hello');
     console.log('asfasfsaf');
 
+    //https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d#:~:text=many%20other%20languages.-,.map(),-Let%20me%20explain
+
     var officers = [
       { id: 20, name: 'Captain Piett' },
       { id: 24, name: 'General Veers' },
@@ -53,13 +55,14 @@ export class AppComponent {
     // What you need
     //[20, 24, 56, 88]
 
-    var officerss = officers.map((x) => x.id);
+    var officerss = officers.map((x) => x.id); //officers IDs
     console.log(officerss);
 
+    //https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d#:~:text=the%20original%20array.-,.reduce(),-Just%20like%20.map
     var pilots = [
       {
         id: 10,
-        name: "Poe Dameron",
+        name: 'Poe Dameron',
         years: 14,
       },
       {
@@ -69,17 +72,49 @@ export class AppComponent {
       },
       {
         id: 41,
-        name: "Tallissan Lintra",
+        name: 'Tallissan Lintra',
         years: 16,
       },
       {
         id: 99,
-        name: "Ello Asty",
+        name: 'Ello Asty',
         years: 22,
-      }
+      },
     ];
 
+    const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years, 0); //pilots age
+    console.log(totalYears);
 
+    //https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d#:~:text=from%20an%20array.-,.filter(),-What%20if%20you
+    var pilotsa = [
+      {
+        id: 2,
+        name: 'Wedge Antilles',
+        faction: 'Rebels',
+      },
+      {
+        id: 8,
+        name: 'Ciena Ree',
+        faction: 'Empire',
+      },
+      {
+        id: 40,
+        name: 'Iden Versio',
+        faction: 'Empire',
+      },
+      {
+        id: 66,
+        name: 'Thane Kyrell',
+        faction: 'Rebels',
+      },
+    ];
+
+    var findPilotsFaction = pilotsa.filter((x) => x.faction === 'Rebels');
+    console.log(findPilotsFaction);
+
+    const rebels = pilotsa.filter((pilot) => pilot.faction === 'Rebels');
+    const empire = pilotsa.filter((pilot) => pilot.faction === 'Empire');
+    console.log(rebels);
   }
 
   getKeys(map) {
